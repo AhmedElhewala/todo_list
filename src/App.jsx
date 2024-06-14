ae840ec86e9366b3793a8187eca1858fe36beb33
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-06-14 18:39:32 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./context/ThemeContext";
-import { useEffect, useState } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useDispatch } from "react-redux";
-import Home from "./pages/Home";
-import Spinner from "./ui/Spinner";
+
+const Home = lazy(() => import("./pages/Home"));
+const Spinner = lazy(() => import("./ui/Spinner"));
+
 import { updateFullList } from "./service/todosSlice";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const stopLoading = () => {
-      setTimeout(() => {
-        setIsLoading(false)
-      }, 2000)
-    }
-    window.addEventListener("load", stopLoading);
-
-    return () => window.removeEventListener("load", stopLoading);
-  },
-  []);
 
   useEffect(() => {
     const todosData = localStorage.getItem("todos");
@@ -33,8 +22,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        {isLoading && <Spinner />}
+      <Suspense fallback={<Spinner />}>
         <Home />
+      </Suspense>
       </ThemeProvider>
     </QueryClientProvider>
   )
